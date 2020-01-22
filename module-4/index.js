26e2d230f7e54be0994917faf4eb550f5926e0d4
josimar-junior <juniorpaz99@gmail.com>
2020-01-21 22:18:10 -0300

    Completing the hero's crud

--- module-4/index.js
+++ module-4/index.js
@@ -10,22 +10,46 @@ async function main() {
 
         .option('-n, --name [value]', "Hero's name")
         .option('-p --power [value]', "Hero's power")
+        .option('-i --id [value]', "Hero's id")
         .option('-s --save', 'Save')
+        .option('-l --list', 'List')
+        .option('-r --remove', 'Remove hero by id')
+        .option('-u --update [value]', 'Update')
 
         .parse(process.argv);
 
     const hero = new Hero(Commander);
 
     try {
-        if(Commander.save) {
+        if (Commander.save) {
+            delete hero.id;
             const result = Database.save(hero);
-            if(!result) {
+            if (!result) {
                 console.log('Error saving hero');
                 return;
             }
-            console.log('Hero saved successfully');
+            console.log('Hero successfully saved');
         }
-    } catch(e) {
+
+        if (Commander.list) {
+            const result = await Database.list();
+            console.log(result);
+            return;
+        }
+
+        if (Commander.remove) {
+            await Database.remove(hero.id);
+            console.log('Hero successfully removed');
+        }
+
+        if (Commander.update) {
+            const idToUpdate = parseInt(Commander.update);
+            const data = JSON.stringify(hero);
+            const heroUpdate = JSON.parse(data);
+            await Database.update(idToUpdate, heroUpdate);
+            console.log('Hero successfully updated');
+        }
+    } catch (e) {
         console.error('Error -> ', e);
     }
 }
